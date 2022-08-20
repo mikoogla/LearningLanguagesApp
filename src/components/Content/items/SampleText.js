b220f99a695ef3857375aa093c15eea08f793e62
mikoogla9 <rgmikoogla@gmail.com>
2022-08-20 18:30:08 +0200

    new sample text

---
+++ src/components/Content/items/SampleText.js
@@ -0,0 +1,7 @@
+const text = `Lorem ipsum dolo. Praesent eleifend lacus eu dolor viverra, sed ullamcorper tellus volutpat. Praesent at odio vel lectus imperdiet pulvinar at eget diam. Nam ac facilisis quam, sit amet hendrerit mauris. Proin viverra sem turpis, non convallis dolor tristique eget. Aliquam cursus et velit ut condimentum. Nullam non est sed enim suscipit efficitur id at risus. Nullam sed odio enim. Curabitur nec dolor sodales justo aliquam sodales.\n
+
+    Praesent id loremorta nisl id lacus condimentum, non maximus erat convallis. Aenean vulputate ligula at orci varius pellentesque. Maecenas et urna sed lectus ultricies dignissim. Mauris sagittis tellus ut commodo efficitur. Suspendisse potenti. Morbi quis vulputate turpis. Vestibulum convallis molestie sem ut lacinia. Nullam tincidunt metus ac lectus porttitor semper. Curabitur porta nulla venenatis lectus sagittis finibus. Sed molestie viverra felis, ut gravida lorem efficitur at. Integer at laoreet sem. Integer non lobortis felis. Ut rutrum felis non sodales rhoncus. Fusce interdum cursus arcu quis interdum.\n
+
+    Ut ultrices est a magna euismod feugiat. Donec vulputate sollicitudin neque eu pharetra. Quisque egestas vulputate justo no`;
+
+export default text;
--- src/components/Content/items/TextContent.js
+++ src/components/Content/items/TextContent.js
@@ -1,32 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Product from "../../Product/Product";
 import Card from "../../UI/Card/Card";
 import styles from "./Styles.module.css";
-
+import text from "./SampleText.js";
+console.log(text);
 export default function TextContent() {
-  const cart = useSelector((state) => state.cart.products);
-  const cartSize = useSelector((state) => state.cart.size);
   const isHidden = useSelector((state) => state.cart.hide);
-  const loading = useSelector((state) => state.sync.loading);
 
   return (
     <>
       {!isHidden && (
         <Card className={styles.content}>
-          {cartSize > 0 ? (
-            loading ? (
-              <h1>Loading</h1>
-            ) : (
-              cart.map((product) => (
-                <Product key={product.id} product={product}></Product>
-              ))
-            )
-          ) : (
-            <div>
-              <h2>You will see your text here</h2>
-            </div>
-          )}
+          <h2>You will see your text here</h2>
+          <Card className={styles.textcontainer}>{text}</Card>
         </Card>
       )}
     </>
