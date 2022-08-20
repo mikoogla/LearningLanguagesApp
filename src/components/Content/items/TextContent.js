c2a8de923459e4e1acb6e69c64aa9e67d05a3204
mikoogla9 <rgmikoogla@gmail.com>
2022-08-20 19:10:38 +0200

    main feature uploaded

--- src/components/Content/items/TextContent.js
+++ src/components/Content/items/TextContent.js
@@ -4,7 +4,31 @@ import Card from "../../UI/Card/Card";
 import styles from "./Styles.module.css";
 import text from "./SampleText.js";
 console.log(text);
+const textSplitOnWords = text.split(" ");
+console.log(textSplitOnWords);
+
 export default function TextContent() {
+  const WordsToLinks = () => {
+    return (
+      <div>
+        {textSplitOnWords.map((word) => {
+          if (word.length % 2 === 0) {
+            return (
+              <a className="known" href={word}>
+                {word}{" "}
+              </a>
+            );
+          } else {
+            return (
+              <a className="unknown" href={word}>
+                {word}{" "}
+              </a>
+            );
+          }
+        })}
+      </div>
+    );
+  };
   const isHidden = useSelector((state) => state.cart.hide);
 
   return (
@@ -12,7 +36,7 @@ export default function TextContent() {
       {!isHidden && (
         <Card className={styles.content}>
           <h2>You will see your text here</h2>
-          <Card className={styles.textcontainer}>{text}</Card>
+          <Card className={styles.textcontainer}>{WordsToLinks()}</Card>
         </Card>
       )}
     </>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
       <h3 className={styles.name}>Learning Languages App</h3>
       <div className={styles.info}>
         <Card className={styles.sum}>
-          Your points: {Math.abs(totalPrice).toFixed(2)}
+          You know {Math.abs(totalPrice).toFixed(0)} words
         </Card>
         <Button
           onClick={() => {
