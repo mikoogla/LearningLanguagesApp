ca226d36cc8a1e0053e1ac2be5293522d3f3adf5
mikoogla9 <rgmikoogla@gmail.com>
2022-08-22 10:20:25 +0200

    github login update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className={styles.main}>
       Copyright © 2022&nbsp;
-      <a alt="mikoogla9" href="https://github.com/mikoogla9">
+      <a alt="mikoogla9" href="https://github.com/mikoogla">
         mikoogla
       </a>
     </div>
