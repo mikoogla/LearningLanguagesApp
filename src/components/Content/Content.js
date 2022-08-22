6d508b8b36ee5ac8a153a580e57a895eaf7684e9
mikoogla9 <rgmikoogla@gmail.com>
2022-08-22 13:42:16 +0200

    styling dictionary

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,10 +2,12 @@ import React from "react";
 import TextContent from "./Text/TextContent";
 import styles from "./Content.module.css";
 import SavedNotes from "./Notes/SavedNotes";
+import Dictionary from "./Dictionary/Dictionary";
 export default function Content() {
   return (
     <div className={styles.main}>
       <TextContent />
+      <Dictionary />
       <SavedNotes />
     </div>
   );
---
+++ src/components/Content/Dictionary/Dictionary.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Card from "../../UI/Card/Card";
+import styles from "./Dictionary.module.css";
+
+export default function Dictionary() {
+  const words = useSelector((state) => state.dictionary.dictionary);
+  return (
+    <Card className={styles.content}>
+      <h1>Dictionary</h1>
+      <h2>Your words:</h2>
+      <div className={styles.wordlist}>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        <div className="known" key={Math.random()}>
+          test
+        </div>
+        {words.map((word) => (
+          <div className={`${word.state}`} key={word.id}>
+            {word.word}
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
+}
