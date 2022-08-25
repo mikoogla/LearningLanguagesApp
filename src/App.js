ad8d36efe67c1712fc76aa25d1f7a5e31a7731b5
mikoogla9 <rgmikoogla@gmail.com>
2022-08-25 22:03:01 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
 import Footer from "./components/Footer/Footer";
 import DictionarySync from "./sync/DictionarySync";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector((state) => state.logic.state);
   return (
     <div className={styles.main}>
-      <header>
-        <Navbar />
-        <DictionarySync />
-      </header>
-      <main>
-        <Content />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+      <Switch>
+        <Route path={`/`}>
+          <Redirect to={`/${state}`} />
+          <header>
+            <Navbar />
+            <DictionarySync />
+          </header>
+          <main>
+            <Content />
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import Button from "../UI/Button/Button";
 import styles from "./Navbar.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { switchState } from "../../store/index.js";
+import { Link } from "react-router-dom";
 import Card from "../UI/Card/Card";
 
 export default function Navbar() {
@@ -24,14 +25,16 @@ export default function Navbar() {
       <h3 className={styles.name}>Learning Languages App</h3>
       <div className={styles.info}>
         <Card className={styles.sum}>You know {stats.known} words</Card>
-        <Button
-          onClick={() => {
-            dispatch(switchState());
-          }}
-          className={styles.button}
-        >
-          {buttonText()}
-        </Button>
+        <Link to={`/${buttonText().toLowerCase()}`}>
+          <Button
+            onClick={() => {
+              dispatch(switchState());
+            }}
+            className={styles.button}
+          >
+            {buttonText()}
+          </Button>
+        </Link>
       </div>
     </div>
   );
